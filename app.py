f5f0ecfcf62d34a1bd3961c446fe2aa6c4dcb3dc
miaonanlin <58052608+miaonanlin@users.noreply.github.com>
2021-05-07 17:45:55 -0400

    d

---
+++ app.py
@@ -0,0 +1,52 @@
+import flask
+import os
+import pickle
+import pandas as pd
+from skimage import io
+from skimage import transform
+
+app = flask.Flask(__name__, template_folder='templates')
+
+path_to_vectorizer = 'models/vectorizer.pkl'
+path_to_text_classifier = 'models/text-classifier.pkl'
+path_to_image_classifier = 'models/image-classifier.pkl'
+
+with open(path_to_vectorizer, 'rb') as f:
+    vectorizer = pickle.load(f)
+
+with open(path_to_text_classifier, 'rb') as f:
+    model = pickle.load(f)
+
+with open(path_to_image_classifier, 'rb') as f:
+    image_classifier = pickle.load(f)
+
+
+
+
+@app.route('/input_values/', methods=['GET', 'POST'])
+def input_values():
+    if flask.request.method == 'GET':
+        # Just render the initial form, to get input
+        return(flask.render_template('input_values.html'))
+
+    if flask.request.method == 'POST':
+        # Get the input from the user.
+        var_one = flask.request.form['input_variable_one']
+        var_two = flask.request.form['another-input-variable']
+        var_three = flask.request.form['third-input-variable']
+
+        list_of_inputs = [var_one, var_two, var_three]
+
+        return(flask.render_template('input_values.html',
+            returned_var_one=var_one,
+            returned_var_two=var_two,
+            returned_var_three=var_three,
+            returned_list=list_of_inputs))
+
+    return(flask.render_template('input_values.html'))
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+sns.set()
+import matplotlib.pyplot as plt
+%matplotlib inline
+# Import the trees from sklearn
+from sklearn import tree
+# Helper function to split our data
+from sklearn.model_selection import train_test_split
+# Helper fuctions to evaluate our model.
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix, f1_score, roc_auc_score
+# Helper function for hyper-parameter turning.
+from sklearn.model_selection import GridSearchCV
+# Import our Decision Tree
+from sklearn.tree import DecisionTreeClassifier
+# Import our Random Forest
+from sklearn.ensemble import RandomForestClassifier
+# Library for visualizing our tree
+# If you get an error, run 'pip install graphviz' in your terminal
+import graphviz
+import pickle
+
+df = pd.read_csv('https://raw.githubusercontent.com/miaonanlin/CTP_DataScience_GroupProject/main/phishing_website_dataset.csv')
+select_features = ['SSLfinal_State','URL_of_Anchor', 'web_traffic',  'having_Sub_Domain','Links_in_tags']
+
+X = df[select_features]
+
+y = df['Result']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=45)
+
+model = RandomForestClassifier()
+
+model.fit(X_train, y_train)
+
+
+#pickle.dump(vectorizer, open('models/vectorizer.pkl', 'wb') )
+
+pickle.dump(model, open('models/text-classifier.pkl', 'wb') )
